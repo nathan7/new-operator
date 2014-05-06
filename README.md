79b03b3fb22dc5a69e5d5d5b9cdf61eb3e842b85
Nathan Zadoks <nathan@nathan7.eu>
2014-05-07 00:18:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# new-operator
+
+  the `new` operator as a function
+
+## Installation
+
+    npm install new-operator
+
+## API
+### new.call(constructor, …arguments)
+### new.apply(constructor, arguments)
---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports =
+function new_() { //jshint validthis:true
+  if (arguments.length === 1) return new this(arguments[0])
+  if (arguments.length === 2) return new this(arguments[0], arguments[1])
+  if (arguments.length === 3) return new this(arguments[0], arguments[1], arguments[2])
+  if (arguments.length === 4) return new this(arguments[0], arguments[1], arguments[2], arguments[3])
+  if (arguments.length === 5) return new this(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4])
+  if (arguments.length === 6) return new this(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5])
+  if (arguments.length === 7) return new this(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6])
+  if (arguments.length === 8) return new this(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6], arguments[7])
+  if (arguments.length === 9) return new this(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6], arguments[7], arguments[8])
+  if (arguments.length === 10) return new this(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6], arguments[7], arguments[8], arguments[9])
+  var obj = Object.create(this.prototype)
+    , ret = this.apply(obj, arguments)
+  return typeof ret === 'object'
+    ? ret
+    : obj
+}
